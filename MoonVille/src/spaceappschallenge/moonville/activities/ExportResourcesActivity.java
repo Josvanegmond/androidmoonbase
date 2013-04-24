82cc1418f399f526d5356b9e2f3283ae852bdf18
egmond-van-jos <jostievanegmond@gmail.com>
2013-04-24 22:59:32 +0200

    Changed interface of export

--- MoonVille/src/spaceappschallenge/moonville/activities/ExportResourcesActivity.java
+++ MoonVille/src/spaceappschallenge/moonville/activities/ExportResourcesActivity.java
@@ -17,28 +17,25 @@ import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class ExportResourcesActivity extends GameActivity {
+public class ExportResourcesActivity extends GameActivity
+{
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_export_resources);
-		Log.i("ExportResourcesActivity", "gathering resources..");
-		ArrayList<Resource> resources = Resources.getInstance()
-				.getAllResources();
+
 		ExportResourceListAdapter resourceListAdapter = new ExportResourceListAdapter();
 		Log.i("ResourcesActivity", "showing export resources screen..");
 		ListView resourceListView = (ListView) this
-				.findViewById(R.id.exportResourceslist);
+				.findViewById(R.id.demandList);
 		resourceListView.setAdapter(resourceListAdapter);
 		updateUI();
 	}
 
-	public void updateUI() {
-		TextView exportBudgetTextView = (TextView) (this
-				.findViewById(R.id.exportBudgetTextView));
-		exportBudgetTextView.setText(""
-				+ MoonBaseManager.getCurrentMoonBase().getMoney());
+	public void updateUI()
+	{
 	}
 
 	@Override
