8aefec6dbb37cd6e32d3be67f8392379d718bb39
egmond-van-jos <jostievanegmond@gmail.com>
2013-04-24 19:42:43 +0200

    Changed showBuildings: already built: opaque unlocked: transparent unavailable: invisible
    added documentation to BaseOverviewActivity

--- MoonVille/src/spaceappschallenge/moonville/activities/BaseOverviewActivity.java
+++ MoonVille/src/spaceappschallenge/moonville/activities/BaseOverviewActivity.java
@@ -113,74 +113,128 @@ public class BaseOverviewActivity extends GameActivity {
 		return true;
 	}
 
-	private void showBuildings() {
-
+	
+	/**
+	 * showBuildings shows the buildings on the moon landscape.
+	 * It's using an absolute layout (deprecated) to be able to position buildings on absolute position.
+	 * RelativeLayout didn't work, the buildings did not scroll along and out of the viewport.
+	 * 
+	 * The method maintains a list of images (buildingImagesList) for each building placed in the absolutelayout.
+	 * When called, this list is cleared, the images removed from layout, and then re-added.
+	 * The image positions are obtained from the Buildings factory, which gets the data from buildings.xml.
+	 * 
+	 * Image transparency is determined by the fact wether or not a building can already be build:
+	 * 	If the building is build, the image is opaque (alpha 100)
+	 * 	Else if all required buildings of a building have been build, the image is transparent (alpha 50)
+	 *  Else building is invisible (alpha 0)
+	 */
+	private void showBuildings()
+	{
+		//erase all building images from list
 		for (ImageView buildingImage : buildingImageList) {
 			((AbsoluteLayout) buildingImage.getParent())
 					.removeView(buildingImage);
 		}
 
+		//clear list
 		buildingImageList.clear();
-
+		
+		
+		//obtain the current moonbase
+		MoonBase moonBase = MoonBaseManager.getCurrentMoonBase();
+		
+		
+		//get all buildings, to check for each building if it is visible and what its position should be
 		List<BuildingDefinition> buildings = Buildings.getInstance().getAllBuildings();
 		
 		for (final BuildingDefinition building : buildings)
 		{
-			ImageView buildingImage = new ImageView(this);
-			
-			buildingImage.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View view)
-				{
-					Intent intent = new Intent(BaseOverviewActivity.this, BuildingInfoActivity.class);
-					intent.putExtra("Building", building.getName());
-					view.getContext().startActivity( intent );
-					BaseOverviewActivity.this.finish();
-				}
-			});
+			//see if the building can be build or not, and whether or not it is already visible
+			boolean canBeBuild = moonBase.canBuild(building.getName());
 
-			buildingImageList.add(buildingImage);
-
-			android.content.res.Resources res = this.getResources();
-
-			int resID = res
-					.getIdentifier("ref_"
-							+ building.getName().replace(" ", "_")
-									.toLowerCase(), "drawable",
-							getPackageName());
-			Drawable buildingDrawable = res.getDrawable(resID);
-			buildingImage.setImageDrawable(buildingDrawable);
-
-			BuildingDefinition bd = Buildings.getInstance().getBuilding(building.getName());
-			AbsoluteLayout.LayoutParams buildingParams = new AbsoluteLayout.LayoutParams(
-					buildingDrawable.getIntrinsicWidth(),
-					buildingDrawable.getIntrinsicHeight(),
-					bd.getXPos() * 2, bd.getYPos() * 2);
-			
-			boolean canBeBuild = MoonBaseManager.getCurrentMoonBase().canBuild(building.getName());
-			
-			//buildingImage.setClickable( canBeBuild );
 			if( canBeBuild == false )
 			{
-				buildingImage.setAlpha( 50 );
-			}
-			else
-			{
-				buildingImage.setAlpha( 100 );
+				//nothing happens, no building visible
 			}
 			
-			buildingImage.setLayoutParams(buildingParams);
+			else
+			{		
+				ImageView buildingImage = new ImageView(this);
+				
+				//by clicking on the building, the BuildingInfoActivity pops up
+				buildingImage.setOnClickListener(new OnClickListener() {
+					@Override
+					public void onClick(View view)
+					{
+						Intent intent = new Intent(BaseOverviewActivity.this, BuildingInfoActivity.class);
+						intent.putExtra("Building", building.getName());
+						view.getContext().startActivity( intent );
+						BaseOverviewActivity.this.finish();
+					}
+				});
+	
+				buildingImageList.add(buildingImage);
+	
+				android.content.res.Resources res = this.getResources();
+	
+				//obtain the bitmap from the name of the building
+				//TODO: replace/remove ref_ prefix to match name of the final image in the file system
+				int resID = res
+						.getIdentifier("ref_"
+								+ building.getName().replace(" ", "_")
+										.toLowerCase(), "drawable",
+								getPackageName());
+				Drawable buildingDrawable = res.getDrawable(resID);
+				buildingImage.setImageDrawable(buildingDrawable);
+	
+				//place it in the background according to position determined in Building object
+				BuildingDefinition bd = Buildings.getInstance().getBuilding(building.getName());
+				AbsoluteLayout.LayoutParams buildingParams = new AbsoluteLayout.LayoutParams(
+						buildingDrawable.getIntrinsicWidth() /2,
+						buildingDrawable.getIntrinsicHeight() /2,
+						bd.getXPos(), bd.getYPos());
+				
+				buildingImage.setLayoutParams(buildingParams);
+	
+				moonSurfaceLayout.addView(buildingImage);
+				
 
-			moonSurfaceLayout.addView(buildingImage);
+				//depending on wether or not it is already build, show it 
+				if( moonBase.getBuilding( building.getName() ) != null )
+				{
+					buildingImage.setAlpha( 100 );
+				}
+				else
+				{
+					buildingImage.setAlpha( 50 );
+				}
+			}
 		}
 	}
 
+	
+
 	// methods called by onClick property of button in xml
-	public void nextTurn(View view) {
+	
+	/**
+	 * nextTurn pushes the game forward to the next month.
+	 * It forwards time, changes resources, builds the
+	 * buildings and divides the power.
+	 * 
+	 * Performed tasks:
+	 * 	divide power among buildings
+	 * 	process all resources
+	 * 	calculate reputation
+	 * 	change month
+	 * 	save game state
+	 * 
+	 */
+	public void nextTurn(View view)
+	{
 		MoonBase moonBase = MoonBaseManager.getCurrentMoonBase();
 		BuildingTree tree = moonBase.getBuiltBuildings();
 		tree.checkPower();
-		tree.checkRequiredBuildings();
+		//tree.checkRequiredBuildings(); ?? what's this doing here? -Jos
 
 		Resources resources = Resources.getInstance();
 		ArrayList<Resource> available = (ArrayList<Resource>) tree
@@ -231,6 +285,7 @@ public class BaseOverviewActivity extends GameActivity {
 	}
 
 	// some scrollbar fix for scrolling
+	//TODO: make a custom widget for this
 	private void fixHVScrollViews() {
 		final HorizontalScrollView hScroll = (HorizontalScrollView) findViewById(R.id.moonsurface_hscrollview);
 		final ScrollView vScroll = (ScrollView) findViewById(R.id.moonsurface_vscrollview);
