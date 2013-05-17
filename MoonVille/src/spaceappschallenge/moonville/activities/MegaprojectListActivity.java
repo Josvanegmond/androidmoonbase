01083241d4bb09c99bd07ecc5247f9e6ec28cc7b
robikshrestha <robikshrestha@gmail.com>
2013-05-17 09:40:35 +0545

    Added megaprojects

---
+++ MoonVille/src/spaceappschallenge/moonville/activities/MegaprojectListActivity.java
@@ -0,0 +1,51 @@
+package spaceappschallenge.moonville.activities;
+
+import java.util.List;
+
+import spaceappschallenge.moonville.R;
+import spaceappschallenge.moonville.domain.BuildingDefinition;
+import spaceappschallenge.moonville.factories.Buildings;
+import spaceappschallenge.moonville.factories.MoonBaseManager;
+import spaceappschallenge.moonville.listadapters.MegaprojectListAdapter;
+import android.os.Bundle;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MegaprojectListActivity extends GameActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_mega_project_list);
+
+		List<BuildingDefinition> megaprojects = Buildings.getInstance()
+				.getAllMegaprojects();
+		MegaprojectListAdapter megaprojectListAdapter = new MegaprojectListAdapter(
+				megaprojects);
+
+		ListView megaprojectsListView = (ListView) this
+				.findViewById(R.id.megaprojectsListView);
+		megaprojectsListView.setAdapter(megaprojectListAdapter);
+		// ApplicationService.setImportResourcesActivity(this);
+		updateUI();
+	}
+	
+	@Override
+	protected void onStart() {
+	   super.onStart();
+	   // In order to not be too narrow, set the window size based on the screen resolution:
+	   final int screen_width = getResources().getDisplayMetrics().widthPixels;
+	   final int new_window_width = screen_width * 90 / 100; 
+	   android.view.WindowManager.LayoutParams layout = getWindow().getAttributes();
+	   layout.width = Math.max(layout.width, new_window_width); 
+	   getWindow().setAttributes(layout);
+	}
+
+	public void updateUI() {
+		TextView budgetTextView = (TextView) (this
+				.findViewById(R.id.megaprojectBudgetTextView));
+		budgetTextView.setText(""
+				+ MoonBaseManager.getCurrentMoonBase().getMoney());
+	}
+
+}
---
+++ MoonVille/src/spaceappschallenge/moonville/domain/BuildingDefinition.java
@@ -0,0 +1,98 @@
+package spaceappschallenge.moonville.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import spaceappschallenge.moonville.miscellaneous.SerializablePair;
+
+/**
+ * Provides access to the xml attributes of a building. This is a read-only
+ * object, and is made by the xml parser
+ */
+public class BuildingDefinition implements Serializable {
+	private final String name;
+	private final String info;
+	private String type;
+	private final List<SerializablePair<Resource, Integer>> requiredResources;
+	private final List<SerializablePair<Resource, Integer>> outputResources;
+	private final List<String> requiredBuildings;
+	private final int inputPower;
+	private final int outputPower;
+	private final int requiredTurns;
+	private final int monetaryCost;
+	private final int xPos, yPos;
+
+	public BuildingDefinition(String name, String info, String type,
+			int inputPower, int outputPower, int monetaryCost,
+			int requiredTurns,
+			ArrayList<SerializablePair<Resource, Integer>> requiredResources,
+			ArrayList<SerializablePair<Resource, Integer>> outputResources,
+			ArrayList<String> requiredBuildings, int xPos, int yPos) {
+		this.name = name;
+		this.info = info;
+		this.type = type;
+		this.inputPower = inputPower;
+		this.outputPower = outputPower;
+		this.monetaryCost = monetaryCost;
+		this.requiredTurns = requiredTurns;
+		this.requiredResources = Collections
+				.unmodifiableList(requiredResources);
+		this.outputResources = Collections.unmodifiableList(outputResources);
+		this.requiredBuildings = Collections
+				.unmodifiableList(requiredBuildings);
+		this.xPos = xPos;
+		this.yPos = yPos;
+	}
+
+	// Setters and Getters
+	public String getName() {
+		return name;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+	
+	public String getType(){
+		return type;
+	}
+
+	public List<SerializablePair<Resource, Integer>> getRequiredResources() {
+		return requiredResources;
+	}
+
+	public List<SerializablePair<Resource, Integer>> getOutputResources() {
+		return outputResources;
+	}
+
+	public List<String> getRequiredBuildings() {
+		return this.requiredBuildings;
+	}
+
+	public int getInputPower() {
+		return inputPower;
+	}
+
+	public int getXPos() {
+		return xPos;
+	}
+
+	public int getYPos() {
+		return yPos;
+	}
+
+	public int getOutputPower() {
+		return outputPower;
+	}
+
+	public int getRequiredTurns() {
+		return requiredTurns;
+	}
+
+	public int getMonetaryCost() {
+		return monetaryCost;
+	}
+
+}
