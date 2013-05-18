2839e26fa2bf1da84578a41fcb9c60016424f041
robikshrestha <robikshrestha@gmail.com>
2013-05-18 08:48:45 +0545

    Added code to fill the resource list item

--- MoonVille/src/spaceappschallenge/moonville/listadapters/ExportResourceListAdapter.java
+++ MoonVille/src/spaceappschallenge/moonville/listadapters/ExportResourceListAdapter.java
@@ -21,48 +21,62 @@ import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ExportResourceListAdapter extends BaseAdapter
-{
-	private List<SerializablePair<Resource,Integer>> resourceList;
+public class ExportResourceListAdapter extends BaseAdapter {
+	private List<SerializablePair<Resource, Integer>> resourceList;
 
-	public ExportResourceListAdapter( List<SerializablePair<Resource,Integer>> resourceList )
-	{
+	public ExportResourceListAdapter(
+			List<SerializablePair<Resource, Integer>> resourceList) {
 		this.resourceList = resourceList;
 	}
 
 	@Override
-	public int getCount()
-	{
+	public int getCount() {
 		return resourceList.size();
 	}
 
 	@Override
-	public Object getItem(int index)
-	{
+	public Object getItem(int index) {
 		return resourceList.get(index);
 	}
 
 	@Override
-	public long getItemId(int index)
-	{
+	public long getItemId(int index) {
 		return index;
 	}
 
 	@Override
-	public View getView(int index, View convertView, ViewGroup parent)
-	{
+	public View getView(int index, View convertView, ViewGroup parent) {
 		Resource resource = this.resourceList.get(index).first;
-
-		if (convertView == null)
-		{
-			LayoutInflater inflater = LayoutInflater.from( parent.getContext() );
-			convertView = inflater.inflate(R.layout.listitem_export_resource, parent, false);
+		int amount = this.resourceList.get(index).second;
+		if (convertView == null) {
+			LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+			convertView = inflater.inflate(R.layout.listitem_export_resource,
+					parent, false);
 		}
 
-		TextView resourceName = (TextView) convertView.findViewById(R.id.exportresourcename);
-		resourceName.setText(resource.getName());
-		
+		TextView resourceDescription = (TextView) convertView
+				.findViewById(R.id.export_resource_description);
+		resourceDescription.setText(amount + " units of " + resource.getName()
+				+ " at $" + resource.getExportPrice() * amount);
+
+		/*
+		 * // Add name TextView resourceName = (TextView) convertView
+		 * .findViewById(R.id.exportresourcename);
+		 * 
+		 * // Add amount TextView resourceAmount = (TextView) convertView
+		 * .findViewById(R.id.exportresourcequantity);
+		 * 
+		 * // Add price TextView resourcePrice = (TextView) convertView
+		 * .findViewById(R.id.exportresourcecost);
+		 * resourceName.setText(resource.getName());
+		 * resourceAmount.setText(""+amount); resourcePrice.setText("" + amount
+		 * * resource.getExportPrice());
+		 */
+
 		return convertView;
 	}
+	
+	
+
 
 }
